573f5fc602f71adec08e765c064169bf096b8b0f
jmoraal <jmoraal@users.noreply.github.com>
2020-11-09 22:22:02 +0100

    First working version of reading xyz

---
+++ Week 1/fileread.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  9 16:30:44 2020
+
+@author: s161981
+"""
+
+import numpy as np
+
+timeSteps = 0
+
+with open("Hydrogen.xyz", "r") as inputFile:
+  nrOfAtoms = int(inputFile.readline()) # now still assuming this is constant throughout the file
+  atomType = []
+  atomPosition = np.empty([nrOfAtoms,3])
+  
+  for line in inputFile:
+      timeSteps += 1 #can probably be replaced
+      for i in range(nrOfAtoms):
+          line = inputFile.readline()
+          splittedLine = line.split()
+          atomType += splittedLine[0]
+          temp = splittedLine[1:4]
+          atomPosition = np.append(atomPosition,temp)
+          try: 
+              next(inputFile) #skips the comment line, maybe add later 
+          except StopIteration: #cuts out of loop if there are no more lines to read
+              break
+         
+      
+      
+     
+atomPosition = atomPosition.reshape(nrOfAtoms,3,timeSteps)
+
+'''
+vragen:
+    - lijkt me handig alles van een tijdstip in een keer als
+      matrix op te slaan (soort slice van de totale positiematrix),
+      kan dat of is het niet efficienter dan per regel?
+    - We weten niet van tevoren hoeveel timesteps er zijn, 
+      maar dat zou wel dingen makkelijker maken. Is het het 
+      waard om aan het begin naar de bestandslengte te kijken?
+
+'''

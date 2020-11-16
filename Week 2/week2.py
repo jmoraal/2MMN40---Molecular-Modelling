82c49a055c43c894ebfaee1585232534c3274e54
SannevanKempen <svkempen@icloud.com>
2020-11-16 15:00:05 +0100

    waterexample

--- Week 2/week2.py
+++ Week 2/week2.py
@@ -1,18 +1,138 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Nov 16 11:46:53 2020
+Created on Mon Nov  9 16:30:44 2020
 
 @author: s161981
+
+NOTE: using tab for indentation
 """
+
 import numpy as np
-import week1 as xyz
 
-def Vbond (r,r0,k):
-    return 0.5*k*(r-r0)**2
+"""
+To do: 
+    Adapt readXYZfile: 
+    - read 1st line of file separately to obtain array dimensions 
+    - take timeStep as argument and only return one [nrOfAtoms, 3] array
+    - reduce memory usage
+"""    
+def readXYZnrAtoms(fileName): 
+    with open(fileName, "r") as inputFile:
+        firstString = inputFile.readline().split()
+        nrOfAtoms = int(firstString[0])
+    return nrOfAtoms
 
-def Fbond (r0,k):
-    return -k*(-r0)
+def readXYZfile(fileName, timeStep): 
+    """Read a .xyz file.
+    
+    Reads entire file for arbitrary number of timesteps
+    INPUT: .xyz file 
+    OUTPUT:  atom types and array of xyz-coord for every atom at every timestep.
+    """
+    lines = []
+    firstColumn = []
 
-Fbond = Fbond(0.5,0.74,24531.0)
+    with open(fileName, "r") as inputFile:
+        for line in inputFile: 
+            splittedLine = line.split()
+            firstColumn.append(splittedLine[0])
+            lines.append(splittedLine[1:4])
+        
+    nrOfAtoms = int(firstColumn[0])
+    #timesteps = int(len(lines)/(nrOfAtoms + 2))
+    #This works because for every block of nrOfAtoms positions, there are two other lines
+    
+    atomTypes = firstColumn[(2+(2+nrOfAtoms)*timeStep):((2+nrOfAtoms)*(timeStep+1))]
+    atomPositions = lines[(2+(2+nrOfAtoms)*timeStep):((2+nrOfAtoms)*(timeStep+1))]
+        
+    atomPositions = np.asarray(atomPositions).astype(np.float)
+    return(atomTypes,atomPositions)
 
-forceA = Fbond 
+
+def distAtTime(positions):
+    """ Computes distances between all atoms at given timestep """
+    diff = positions - positions[:,np.newaxis]
+    dist = np.linalg.norm(diff,axis = 2)
+    return(dist)
+
+#testDist = distAtTime(testPos)
+
+""" Also an option, but now still inefficient because it reads
+    the entire file and then takes one slice of the resulting array
+def distAtTimeFromFile(fileName,timestep):
+    posAtTime = readXYZfile(fileName)[1][timestep]
+    diff = posAtTime - posAtTime[:,np.newaxis]
+    dist = np.linalg.norm(diff,axis = 2)
+    return(dist)
+"""
+
+types, xyzs = readXYZfile("hydrogenSmall.xyz", 1)
+print(xyzs)
+
+k = 24531
+r0 = 0.074 # in nm
+xyzs = xyzs/10 # in nm
+print(xyzs)
+
+def Vbond(r):
+    return(1/2*k*(r-r0)**2)
+
+def Fbond(r):
+    return(-k*(r-r0))
+
+def FBondOnAtoms(a,b):
+    r = np.linalg.norm(a-b)
+    print(r)
+    print(a-b)
+    print(np.linalg.norm(a-b))
+    Fa = Fbond(r)*(a-b)/np.linalg.norm(a-b)
+    return(Fa, -Fa)
+
+print(FBondOnAtoms(xyzs[0],xyzs[1]))
+
+# water example
+types, xyzs = readXYZfile("test.xyz", 0)
+
+k = 502416/100
+r0 = 0.9572
+kt = 628.02
+t0 = 104.52
+
+def Vangle(t):
+    return 1/2*kt*(t-t0)**2
+
+def Fangle(t):
+    return -kt*(t-t0)
+
+def FAngleOnAtoms(a,b,c):
+    t = np.dot((a-b),(b-c))/(np.linalg.norm(a-b)*np.linalg.norm(b-c))
+    rAB = np.linalg.norm(a-b)
+    rBC = np.linalg.norm(b-c)
+    print(a-b)
+    print(b-c)
+    # print(t)
+    # print(rAB)
+    normalVecA = np.cross(b-a,np.cross(b-a,b-c))
+    normalVecC = np.cross(c-b,np.cross(b-a,b-c))
+    Fa = Fangle(t)*normalVecA/(np.linalg.norm(normalVecA)*rAB)
+    Fc = Fangle(t)*normalVecC/(np.linalg.norm(normalVecC)*rBC)
+    forces = np.asarray([Fa, -Fa-Fc, Fc])
+    return(forces)
+
+# print(FBondOnAtoms(xyzs[0],xyzs[1]))
+# print("...")
+print(FAngleOnAtoms(xyzs[0], xyzs[1], xyzs[2]))
+# FAngleOnAtoms(xyzs[0], xyzs[1], xyzs[2])
+
+
+
+
+
+
+
+
+
+
+
+
+

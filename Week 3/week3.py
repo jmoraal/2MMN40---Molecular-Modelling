2d0727f4c17807f931affdf773c048d424c618cd
SannevanKempen <svkempen@icloud.com>
2020-11-23 14:38:28 +0100

    week 3 update

--- Week 3/week3.py
+++ Week 3/week3.py
@@ -59,25 +59,25 @@ def distAtTime(positions):
 # - wat Ruben zei in Teams over volgorde van atomen (in group X donderdag 19 nov)
 
 # BOND
-def Vbond(r):
+def Vbond(r, k, r0):
     return(1/2*k*(r-r0)**2)
 
-def Fbond(r):
+def Fbond(r, k, r0):
     return(-k*(r-r0))
 
-def FBondOnAtoms(a,b):
+def FBondOnAtoms(a, b, k, r0):
     r = np.linalg.norm(a-b)
-    Fa = Fbond(r)*(a-b)/np.linalg.norm(a-b)
+    Fa = Fbond(r, k, r0)*(a-b)/np.linalg.norm(a-b)
     return(np.asarray([Fa, -Fa]))
 
 # ANGLE
-def Vangle(t):
+def Vangle(t, kt, t0):
     return 1/2*kt*(t-t0)**2
 
-def Fangle(t):
+def Fangle(t, kt, t0):
     return -kt*(t-t0)
 
-def FAngleOnAtoms(a,b,c):
+def FAngleOnAtoms(a, b, c, kt, t0):
     """Compute angular forces on 3-body atom.
     
     Mind the order of the arguments. The middle argument is supposed to be the middle atom (O in water).
@@ -87,15 +87,15 @@ def FAngleOnAtoms(a,b,c):
     t = np.arccos(np.dot((a-b),(c-b))/(np.linalg.norm(a-b)*np.linalg.norm(c-b)))
     normalVecA = np.cross(a-b,np.cross(a-b,c-b))
     normalVecC = np.cross(b-c,np.cross(a-b,c-b))
-    Fa = Fangle(t)/np.linalg.norm(a-b) * normalVecA/np.linalg.norm(normalVecA)
-    Fc = Fangle(t)/np.linalg.norm(c-b) * normalVecC/np.linalg.norm(normalVecC)
+    Fa = Fangle(t, kt, t0)/np.linalg.norm(a-b) * normalVecA/np.linalg.norm(normalVecA)
+    Fc = Fangle(t, kt, t0)/np.linalg.norm(c-b) * normalVecC/np.linalg.norm(normalVecC)
     return(np.asarray([Fa, -Fa-Fc, Fc]))
 
-def FTotalOnAtoms(a,b,c):
+def FTotalOnAtoms(a,b,c, k, r0, kt, t0):
     """Compute total forces on 3-body atom."""
-    FBondAB = FBondOnAtoms(a,b)
-    FBondBC = FBondOnAtoms(b,c)
-    FAngle = FAngleOnAtoms(a,b,c)
+    FBondAB = FBondOnAtoms(a,b,k,r0)
+    FBondBC = FBondOnAtoms(b,c,k,r0)
+    FAngle = FAngleOnAtoms(a,b,c,kt,t0)
     Fa = FBondAB[0] + FAngle[0]
     Fc = FBondBC[1] + FAngle[2]
     Fb = FBondAB[1] + FBondBC[0] + FAngle[1]
@@ -106,7 +106,7 @@ types, xyzs = readXYZfile("HydrogenSingle.xyz", 0)
 k = 24531/(10**2) # in kJ / (mol A^2)
 r0 = 0.74 # in Angstrom
 
-print(FBondOnAtoms(xyzs[0],xyzs[1]))
+print(FBondOnAtoms(xyzs[0],xyzs[1], k, r0))
 
 # water example
 types, xyzs = readXYZfile("WaterSingle.xyz", 0)
@@ -115,4 +115,84 @@ r0 = 0.9572 # in Angstrom
 kt = 628.02
 t0 = np.deg2rad(104.52)
 
-print(FTotalOnAtoms(xyzs[1] , xyzs[0], xyzs[2]))
+print(FTotalOnAtoms(xyzs[1] , xyzs[0], xyzs[2], k, r0, kt, t0))
+
+
+
+### WEEK 3 ###
+
+def integratorEuler(x, v, a, m, k, r0, dt):
+    """ Implementation of a single step for this integrator. """ 
+    x = x + dt*v + (dt**2)/2*FBondOnAtoms(x[0], x[1], k, r0)/m
+    v = v + dt*(dt**2)/2*FBondOnAtoms(x[0], x[1], k, r0)/m
+    return(x, v, a)
+
+# Generate a random velocity:
+# first get a random unit vector (direction) 
+
+
+# H2 example
+time = 0
+endTime = 10
+types, x = readXYZfile("HydrogenSingle.xyz", 0)
+k = 24531/(10**2) # in kJ / (mol A^2)
+r0 = 0.74 # in Angstrom
+x = xyzs
+u1 = np.random.uniform(size=3)
+u2 = np.random.uniform(size=3)
+u1 /= np.linalg.norm(u1) # normalize
+u2 /= np.linalg.norm(u2)
+v1 = 0.01*u1
+v2 = 0.01*u2
+v = np.array(v1,v2)
+m = 1.00784
+a = FBondOnAtoms(x[0],x[1])/m
+dt = 1
+
+while(time<=endTime):
+    x, v, a = integratorEuler(x, v, a, m, k, r0, dt) 
+    time += dt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

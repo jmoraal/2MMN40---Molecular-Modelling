73c984b5d46388b62784856a0e694ff848d5368e
jmoraal <jmoraal@users.noreply.github.com>
2021-01-20 12:12:26 +0100

    Create function to find good boxsizes
    'Good' means no grid points are left empty, so molecules have best possible distributed positions

--- Final Project/GenerateTopology.py
+++ Final Project/GenerateTopology.py
@@ -189,7 +189,6 @@ def writeMixtureTopology(nrOfMolecules, boxSize):
 ### Generate xyz's ###
 
 def createGrid(nrOfMolecules, boxSize):
-    global rand
     n = int(nrOfMolecules**(1/3)) + 1
     d = boxSize / n
     rand = []
@@ -284,9 +283,29 @@ def writeConfig(type, boxSize):
         writeMixtureXYZ(nrOfMolecules, boxSize)
     else: 
         print('Type unknown, try again')
-            
-writeConfig('mixture', 29) #works reasonably well for uniform placement few open spots
-# writeConfig('mixture', 48.42) #works very well
 
-writeConfig('water', 29)
-writeConfig('ethanol', 29)
+def findSize(approxSize, substance):
+    ''' returns boxsizes so that all grid positions are filled '''
+    sizes = np.arange(approxSize - 3.0, approxSize + 3.0, 0.01)
+    d = []
+    for boxSize in sizes: 
+        if (substance == 'water'):
+            nrOfMolecules = int(33.328*((boxSize/10)**3)) # water
+        elif(substance == 'ethanol'): 
+            nrOfMolecules = int(10.272*((boxSize/10)**3)) # ethanol
+        elif(substance == 'mixture'):
+            nrOfMolecules = int(29.728*((boxSize/10)**3)) # mixture
+        else: 
+            print('Substance not known')            
+        
+        n = int(nrOfMolecules**(1/3)) + 1
+        if (n**3 - nrOfMolecules ==0): 
+            d.append(boxSize)
+    return d
+
+#print(findSize(30,'ethanol'))
+
+writeConfig('water', 31.08)
+writeConfig('ethanol', 32.22)
+writeConfig('mixture', 32.29)
+

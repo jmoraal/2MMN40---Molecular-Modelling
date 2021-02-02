fbff4f1f569b355ce217d1451ed7ba89ae9ad7f2
jmoraal <jmoraal@users.noreply.github.com>
2021-02-02 11:45:11 +0100

    Add functions to plot energy levels

--- Final Project/energyAnalysis.py
+++ Final Project/energyAnalysis.py
@@ -10,7 +10,7 @@ from matplotlib import pyplot as plt
 
 def readMeasurables(fileName): 
     global lines
-    """Read a measurables file. Or: Les Measurables?"""
+    """Read a measurables file. (Les Measurables, haha)"""
     with open(fileName, "r") as inputFile:
         lines = inputFile.readlines()
         
@@ -26,7 +26,48 @@ def readMeasurables(fileName):
         
         for i in range(0,nrTimesteps):
             (kinetic[i],  bondPot[i], anglePot[i], dihedralPot[i], LJpot[i], tempBefore[i], tempAfter[i]) = lines[i+1].split()
-                 
-    return(kinetic,  bondPot, anglePot, dihedralPot, LJpot, tempBefore, tempAfter)
+            
+    #IMPORTANT: for older runs, return MINUS dihedralPot! Original code contained wrong sign
+    return(kinetic,  bondPot, anglePot, -dihedralPot, LJpot, tempBefore, tempAfter)
     
-kinetic, bondPot, anglePot, dihedralPot, LJpot, tempBefore, tempAfter = readMeasurables("Ethanol32.22ThermostatMeasurables.txt")
+#kinetic, bondPot, anglePot, dihedralPot, LJpot, tempBefore, tempAfter = readMeasurables("Ethanol32.22ThermostatMeasurables.txt")
+
+
+### PLOT ENERGY ###
+def plotEnergy(fileName, dt): 
+    measurables = readMeasurables(fileName)
+    Ekin = measurables[0]
+    Epot = np.transpose(np.array(measurables[1:4]))
+    EpotTotal = np.sum(Epot, axis = 1)
+    plt.clf() # Clears current figure
+    t = np.arange(0,len(Ekin)*dt, dt)
+    Etot = np.array(Ekin) + np.array(EpotTotal)
+    plt.plot(t, Ekin, label = 'Kinetic energy')
+    plt.plot(t, EpotTotal, label = 'Potential energy')
+    plt.plot(t, Etot, label = 'Total energy')
+    plt.title('Energy in the system')
+    plt.xlabel('Time (ps)')
+    plt.ylabel('Energy (AMU Å² / ps²)')
+    plt.legend()
+    plt.show()
+    plt.savefig(fileName + "Energies.pdf")
+    
+plotEnergy("Ethanol32.22ThermostatMeasurables.txt", 0.003)
+
+
+def plotPotentials(fileName, dt): 
+    global Epot
+    measurables = readMeasurables(fileName)
+    Epot = np.transpose(np.array(measurables[1:5]))
+    #EpotTotal = np.sum(Epot, axis = 1)
+    plt.clf() # Clears current figure
+    t = np.arange(0,len(Epot)*dt, dt)
+    plt.plot(t, Epot)
+    plt.title('Levels of potential energy')
+    plt.xlabel('Time (ps)')
+    plt.ylabel('Energy (AMU Å² / ps²)')
+    plt.legend( ('Bonds', 'Angles', 'Dihedrals', 'Lennard-Jones'), loc = 'best', bbox_to_anchor=(0.5, 0., 0.5, 0.5))
+    plt.show()
+    plt.savefig(fileName + "Potentials.pdf")
+    
+plotPotentials("Ethanol32.22ThermostatMeasurables.txt", 0.003)
